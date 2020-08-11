d35bd3234b68585d6bdf71781c87bc49c5312060
George Linkovsky <krougz@live.ru>
2020-08-11 17:56:07 +0400

    Autoload fix

--- app/autoload.php
+++ app/autoload.php
@@ -1,17 +1,11 @@
 <?php
 
-function loadModel($class) {
-    
+
+
+spl_autoload_register(function($class) {
     if (file_exists(MODELS_PATH . $class .'.php')) {
         require_once MODELS_PATH . $class .'.php';
     } elseif (file_exists(CONTROLLERS_PATH . $class .'.php')) {
         require_once CONTROLLERS_PATH . $class .'.php';
     }
-}
-
-function loadController($class) {
-    
-}
-
-spl_autoload_register('loadModel');
-spl_autoload_register('loadController');
+});
