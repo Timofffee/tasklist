e435f95336116b55fabde1db7113e8de9f592a1e
George Linkovsky <krougz@live.ru>
2020-08-11 20:45:40 +0400

    Update 404 page style

--- app/views/errors/404.view.php
+++ app/views/errors/404.view.php
@@ -1,5 +1,8 @@
-<div class="column">
-    <h1><sup>wO</sup>ops..  (・_・;)</h1>
-    <p>This doesn't seem to be what you were looking for...</p>
-    <a href="/">Go Home</a>
+
+<div class="container">
+    <div class="column">
+        <h1><sup>wO</sup>ops..  (・_・;)</h1>
+        <p>This doesn't seem to be what you were looking for...</p>
+        <a href="/">Go Home</a>
+    </div>
 </div>
